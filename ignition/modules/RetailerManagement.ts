3f7de4b500a517acee0d41367c74bab9a9b1157c
Jeremiah Samuel <livinalt@gmail.com>
2024-11-28 12:33:16 +0100

    updated contract and addresses

--- ignition/modules/RetailerManagement.ts
+++ ignition/modules/RetailerManagement.ts
@@ -7,8 +7,8 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 const  RetailerManagementModule = buildModule("RetailerManagementModule", (m) => {
     
   const userRoleAddress = "0x65f34d19a3B47c0177661C32497470543816C40c";
-  const productManagementAddress = "0x4456ce0eBadB36Ad298Ff19ce4aC18075c4407Cb";
-  const distributorManagementAddress = "0x7946a63a691555eA75736cDEd41d036C63734881";
+  const productManagementAddress = "0x3D8ceA276f85Dd8b373f2Db22Ac3b4A870cF15ae";
+  const distributorManagementAddress = "0x00fCCefe9eD0B3Fb38a8D1B668302ce194e0b58C";
 
   const retailerManagement = m.contract("RetailerManagement", [userRoleAddress, productManagementAddress, distributorManagementAddress]);
 
